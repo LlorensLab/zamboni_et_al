55a17e0b5cf24c849a8f1a0674461f46eda33c75
Margherita Zamboni <margherita.zamboni@ki.se>
2024-10-21 11:35:06 +0200

    added code for differential analysis (celltypes and injury)

---
+++ code/differential_analysis.R
@@ -0,0 +1,108 @@
+#differential analysis
+source("../source.R")
+multiome <- readRDS("merged_multiome.rds")
+
+clusters <- Idents(multiome) %>% unique()
+
+# enriched genes and regions per cell type 
+# run with wsnn_res_1.1 for the cell subtypes in fig1
+DefaultAssay(multiome) <- "RNA"
+for(i in clusters){
+  markers_cl <- FindMarkers(multiome, 
+                            ident.1 = i, 
+                            only.pos = TRUE,
+                            min.pct = 0.05
+  )
+  
+  markers_cl$gene <- rownames(markers_cl) # add column "gene"
+  markers_cl$comparison <- i
+  markers_rna[[i]] <- markers_cl
+}
+names(markers_rna) <- clusters
+
+DefaultAssay(multiome) <- "peaks"
+for(i in clusters){
+  markers_cl <- FindMarkers(multiome, 
+                            ident.1 = i, 
+                            only.pos = TRUE,
+                            min.pct = 0.05
+  )
+  
+  markers_cl$peak <- rownames(markers_cl)
+  markers_cl$comparison <- i
+  markers_peaks[[i]] <- markers_cl
+}
+names(markers_peaks) <- clusters
+
+
+for(i in clusters){
+  markers_peaks[[i]] <- markers_peaks[[i]] %>%
+    dplyr::filter(p_val_adj < 0.05)
+  
+  markers_rna[[i]] <- markers_rna[[i]] %>%
+    dplyr::filter(p_val_adj < 0.05)
+}
+
+#save 
+saveRDS(markers_rna, "markers_rna_multiome_celltypes.rds")
+saveRDS(markers_peaks, "markers_peaks_multiome_celltypes.rds")
+
+
+# differential analysis injury
+multiome$condition <- factor(multiome$condition,
+                             levels = c("U", "1dpi", "3dpi", "7dpi", "28dpi"))
+
+markers_Uvsothers_peaks <- list()
+markers_Uvsothers_peaks_all <- list()
+for(i in clusters){
+  celltype <- subset(multiome, ident = i)
+  Idents(celltype) <- "condition"
+  DefaultAssay(celltype) <- "peaks"
+  
+  for(j in c("1dpi", "3dpi", "7dpi", "28dpi")){
+    markers_Uvsothers_peaks[[j]] <- FindMarkers(celltype, 
+                                                ident.1 = j,
+                                                ident.2 = "U",
+                                                only.pos = FALSE,
+                                                min.pct = 0.05)
+    markers_Uvsothers_peaks[[j]]$comparison <- paste0(i, "_", j)
+    markers_Uvsothers_peaks[[j]]$peak <- rownames(markers_Uvsothers_peaks[[j]])
+    
+  }
+  markers_Uvsothers_peaks_all[[i]] <- do.call("rbind", markers_Uvsothers_peaks)
+  rm(celltype)
+}
+
+markers_Uvsothers_rna <- list()
+markers_Uvsothers_rna_all <- list()
+for(i in clusters){
+  celltype <- subset(multiome, ident = i)
+  Idents(celltype) <- "condition"
+  DefaultAssay(celltype) <- "RNA"
+  
+  for(j in c("1dpi", "3dpi", "7dpi", "28dpi")){
+    markers_Uvsothers_rna[[j]] <- FindMarkers(celltype, 
+                                              ident.1 = j,
+                                              ident.2 = "U",
+                                              only.pos = FALSE,
+                                              min.pct = 0.05, 
+                                              logfc.threshold = 0.32)
+    markers_Uvsothers_rna[[j]]$comparison <- paste0(i, "_", j)
+    markers_Uvsothers_rna[[j]]$gene <- rownames(markers_Uvsothers_rna[[j]])
+    
+  }
+  markers_Uvsothers_rna_all[[i]] <- do.call("rbind", markers_Uvsothers_rna)
+  rm(celltype)
+}
+
+for(i in clusters){
+  markers_Uvsothers_peaks_all[[i]] <- markers_Uvsothers_peaks_all[[i]] %>%
+    dplyr::filter(p_val < 0.05 & abs(avg_log2FC) > 0.5)
+  
+  markers_Uvsothers_rna_all[[i]] <- markers_Uvsothers_rna_all[[i]] %>%
+    dplyr::filter(p_val < 0.05 & abs(avg_log2FC) > 0.5)
+}
+
+#save
+saveRDS(markers_Uvsothers_rna_all, "markers_Uvsothers_rna_all.rds")
+saveRDS(markers_Uvsothers_peaks_all, "markers_Uvsothers_peaks_all.rds")
--- code/preprocessing/merge_sample_objects.R
+++ code/preprocessing/merge_sample_objects.R
@@ -3,36 +3,37 @@ source("../source.R")
 object <- readRDS("unmerged_obj_multiome_clean.rds")
 
 # merge objects
-combined <- merge(
+multiome <- merge(
   x = object[[1]],
   y = c(object[2:21])
 )
 
 rm(object)
 
-combined$experiment <- strsplit(combined$sample, "_") %>% sapply("[[", 1)
-combined$condition <- strsplit(combined$sample, "_") %>% sapply("[[", 2)
-combined$batch <- strsplit(combined$sample, "_") %>% sapply("[[", 3)
+multiome$experiment <- strsplit(multiome$sample, "_") %>% sapply("[[", 1)
+multiome$condition <- strsplit(multiome$sample, "_") %>% sapply("[[", 2)
+multiome$batch <- strsplit(multiome$sample, "_") %>% sapply("[[", 3)
 
 #remove doublets
-combined <- subset(combined, subset = DF_doublet == "Singlet")
+multiome <- subset(multiome, subset = DF_doublet == "Singlet")
 
-# process RNA and mpeaks
-DefaultAssay(combined) <- "RNA"
-combined <- combined %>% 
+
+# process RNA and peaks
+DefaultAssay(multiome) <- "RNA"
+multiome <- multiome %>% 
   NormalizeData() %>% 
   FindVariableFeatures() %>% 
   ScaleData() %>% 
   RunPCA()
 
-DefaultAssay(combined) <- "cpeaks"
-combined <- combined%>%
+DefaultAssay(multiome) <- "peaks"
+multiome <- multiome%>%
   FindTopFeatures(min.cutoff = "q75") %>%
   RunTFIDF() %>%
   RunSVD()
 
 # build a joint neighbor graph and UMAP visualization using both assays
-combined <- combined %>%
+multiome <- multiome %>%
   FindMultiModalNeighbors(
     reduction.list = list("pca", "lsi"), 
     dims.list = list(1:30, 2:30),
@@ -46,12 +47,64 @@ combined <- combined %>%
     algorithm = 3, 
     resolution = 0.2)
 
-combined <- RunUMAP(combined, reduction = 'pca', dims = 1:30, assay = 'RNA', 
+multiome <- RunUMAP(multiome, reduction = 'pca', dims = 1:30, assay = 'RNA', 
                     reduction.name = 'rna.umap', reduction.key = 'rnaUMAP_')
-combined <- RunUMAP(combined, reduction = 'lsi', dims = 2:30, assay = 'peaks', 
+multiome <- RunUMAP(multiome, reduction = 'lsi', dims = 2:30, assay = 'peaks', 
                     reduction.name = 'lsi.umap', reduction.key = 'lsiUMAP_')
-combined <- FindClusters(combined,
-                         graph.name = "wsnn", 
-                         algorithm = 3, 
-                         resolution = 0.2)
+
+# annotate clusters
+Idents(multiome) <- "wsnn_res.0.1"
+multiome <- RenameIdents(multiome, 
+                         "0" = "Oligodendrocytes", 
+                         "1" = "Microglia",
+                         "2" = "Ependymal",
+                         "3" = "Neurons_V",
+                         "4" = "Macrophages",
+                         "5" = "Astrocytes", "6" = "Astrocytes",
+                         "7" = "Neurons_D", "8" = "Neurons_D", "13" = "Neurons_D", "15" = "Neurons_D","11" = "Neurons_D",
+                         "9" = "OPCs",
+                         "10" = "Perivascular", #Pericytes
+                         "14" = "Perivascular", #Lepto
+                         "12" = "Endothelial")
+
+Idents(multiome) <- factor(x = Idents(multiome), 
+                           levels = c("Neurons_V", "Neurons_D",
+                                      "Astrocytes", "Ependymal",
+                                      "OPCs", "Oligodendrocytes",
+                                      "Microglia", "Macrophages", 
+                                      "Perivascular", "Endothelial"))
+multiome$cluster_ids <- Idents(multiome)
+
+
+#clustering modalities (RNA only, ATAC only, multimodal)
+#fig 1
+multiome <- multiome %>% 
+  FindNeighbors(
+    reduction = "pca",  
+    dims = 1:30,
+    graph.name = "RNA.weight") %>%
+  RunUMAP(
+    dims = 1:30,
+    reduction.name = "rna.umap", 
+    reduction.key = "rnaUMAP_") %>%
+  FindClusters(
+    graph.name = "RNA.weight", 
+    algorithm = 3, 
+    resolution = 1.1)
+
+multiome <- multiome %>%
+  FindNeighbors(
+    reduction = "lsi",  
+    dims = 2:30,
+    graph.name = "ATAC.weight") %>%  
+  RunUMAP(
+    dims = 2:30,
+    reduction.name = "lsi.umap", 
+    reduction.key = "lsiUMAP_") %>%
+  FindClusters(
+    graph.name = "ATAC.weight", 
+    algorithm = 3, 
+    resolution = 1.1)
+
+saveRDS(multiome, "merged_multiome.rds")
 
